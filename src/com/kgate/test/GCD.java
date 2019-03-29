fa893aa964cda31ff73ae178ef6c7de529f9c39f
Gulrez <gulfarooqui1@gmail.com>
2019-03-29 17:40:13 +0530

    sd

--- src/com/kgate/test/GCD.java
+++ src/com/kgate/test/GCD.java
@@ -9,8 +9,13 @@ public class GCD {
 		System.out.println("Enter two number for find GCD.");
 		int n1 = scanner.nextInt();
 		int n2 = scanner.nextInt();
-		
-//		for(int i=1;i<=n1&&i<=)
-				
+		int gcd = 1;
+		for (int i = 1; i <= n1 && i <= n2; i++) {
+			if (n1 % i == 0 && n2 % i == 0) {
+				gcd = i;
+			}
+		}
+		System.out.println(gcd);
+
 	}
 }
